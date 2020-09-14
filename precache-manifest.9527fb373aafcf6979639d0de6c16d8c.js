77460b2d6c61d837372b3eeec9437d009da6ca93
welisonmenezes <welisonmenezes@hotmail.com>
2020-09-14 20:33:49 -0300

    Updates

---
+++ precache-manifest.9527fb373aafcf6979639d0de6c16d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa63db694af27443149fa384cf42344",
+    "url": "/react-js-calculator/index.html"
+  },
+  {
+    "revision": "9a9cbe5994e1cd48ca4b",
+    "url": "/react-js-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "239aaff2ca8196a1edfe",
+    "url": "/react-js-calculator/static/js/2.8904271b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-calculator/static/js/2.8904271b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9cbe5994e1cd48ca4b",
+    "url": "/react-js-calculator/static/js/main.cdf2c410.chunk.js"
+  },
+  {
+    "revision": "fc34924ecb9645d2907c",
+    "url": "/react-js-calculator/static/js/runtime-main.c3c983e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
