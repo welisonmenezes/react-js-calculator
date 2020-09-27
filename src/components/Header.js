64b0c71e0ce53f59dabd2cde1483813fcbb9e653
welisonmenezes <welisonmenezes@hotmail.com>
2020-09-27 13:06:57 -0300

    Add h1 element into the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div className="header">
             <img src={logo} alt="Logo" />
-            React Js Calculator
+            <h1>React Js <span>Calculator</span></h1>
         </div>
     );
 }
